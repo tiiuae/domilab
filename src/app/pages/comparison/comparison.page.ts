747cb1ac77503ad1cea345a5f60b6f3515cb051d
sabughazal <sultan.abughazal@tii.ae>
2024-01-26 18:15:59 +0400

    Update side-by-side attacks page

--- src/app/pages/comparison/comparison.page.ts
+++ src/app/pages/comparison/comparison.page.ts
@@ -10,13 +10,29 @@ import { MenuService } from 'src/app/services/menu.service';
 })
 export class ComparisonPage implements OnInit, AfterViewInit {
 
-  @ViewChild('graphOne') graph_one: any;
-  @ViewChild('graphTwo') graph_two: any;
-  network: any = {nodes: [], links: []}
-  centrality: any;
+  centralities: any[] = []
+  networks: any[] = []
+
+  @ViewChild('graphOne') graphOne: any;
+  @ViewChild('graphTwo') graphTwo: any;
+
+  networkOne: any = {nodes: [], links: []}
+  centralityOne: any;
+  networkTwo: any = {nodes: [], links: []}
+  centralityTwo: any;
 
   selectedNetwork: any = "dolphin"
-  selectedCentrality: any = "pagerank"
+  selectedCentralityOne: any = "pagerank"
+  selectedCentralityTwo: any = "betweenness"
+
+  currentGraphOneLCC: number = 1
+  currentGraphTwoLCC: number = 1
+
+  attackInterval: any = null;
+  attackIntervalDelay: number = 250; // in milliseconds
+  currentIteration: number = 0
+  playing: boolean = false
+  resetEnabled: boolean = true
 
   constructor(
     private api: ApiService,
@@ -26,17 +42,124 @@ export class ComparisonPage implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this.menu.setActivePage('comparison')
+    this._loadNetworksList()
+    this._loadCentralitiesList()
   }
 
   ngAfterViewInit(): void {
-    this.loadNetwork()
+    this.loadBothNetworks()
   }
 
-  async loadNetwork() {
-    this.network = null
-    this.centrality = null
-    this.network = await firstValueFrom(this.api.getNetwork(this.selectedNetwork))
-    this.centrality = await firstValueFrom(this.api.getCentrality(this.selectedNetwork, this.selectedCentrality))
+  async loadBothNetworks() {
+    this.networkOne = null
+    this.networkTwo = null
+    this.centralityOne = null
+    this.centralityTwo = null
+    this.networkOne = await firstValueFrom(this.api.getNetwork(this.selectedNetwork))
+    this.networkTwo = await firstValueFrom(this.api.getNetwork(this.selectedNetwork))
+    this.centralityOne = await firstValueFrom(this.api.getCentrality(this.selectedNetwork, this.selectedCentralityOne))
+    this.centralityTwo = await firstValueFrom(this.api.getCentrality(this.selectedNetwork, this.selectedCentralityTwo))
+  }
+
+
+  // onclick event handler
+  public play(): void {
+    this.playing = true
+    this.attackInterval = setInterval(() => {
+      this.step();
+    }, this.attackIntervalDelay);
+  }
+
+  // onclick event handler
+  public step(): void {
+    let removeNodeId = null
+    removeNodeId = this.centralityOne['nodes'][this.currentIteration]
+    this.graphOne.removeNode(removeNodeId)
+
+    removeNodeId = this.centralityTwo['nodes'][this.currentIteration]
+    this.graphTwo.removeNode(removeNodeId)
+
+    this.currentIteration++
+    this.currentGraphOneLCC = this.centralityOne['largest_component'][this.currentIteration] ? this.centralityOne['largest_component'][this.currentIteration] : 0
+    this.currentGraphTwoLCC = this.centralityTwo['largest_component'][this.currentIteration] ? this.centralityTwo['largest_component'][this.currentIteration] : 0
+  }
+
+  // onclick event handler
+  public reset() {
+    this.resetEnabled = false;
+    this.pause();
+    this.loadBothNetworks();
+    this.resetEnabled = true;
+    this.graphOne.resetGraph();
+    this.graphTwo.resetGraph();
+  }
+
+  // onclick event handler
+  public pause(): void {
+    clearInterval(this.attackInterval)
+    this.attackInterval = null
+    this.playing = false
+  }
+
+  // onclick event handler
+  public selectNetwork(e: any) {
+    this.selectedNetwork = e.target.value.length ? e.target.value : null
+    // this.selectedSigma = null
+    // // this.selectedCentrality = null
+    // this.pause()
+    this.loadBothNetworks()
+    this.graphOne.resetGraph();
+    this.graphTwo.resetGraph();
+  }
+
+  // onclick event handler
+  public selectCentralityOne(e: any) {
+    this.selectedCentralityOne = e.target.value.length ? e.target.value : null
+    // this.selectedSigma = null
+    // this.pause()
+    this.loadBothNetworks()
+    this.graphOne.resetGraph();
+  }
+
+  // onclick event handler
+  public selectCentralityTwo(e: any) {
+    this.selectedCentralityTwo = e.target.value.length ? e.target.value : null
+    // this.selectedSigma = null
+    // this.pause()
+    this.loadBothNetworks()
+    this.graphTwo.resetGraph();
+  }
+
+  public hasSigmaParameter(centrality: any) {
+    return Object.hasOwn(centrality, 'has_sigma')
+  }
+
+  // onupdate event handler
+  public updateSpeed(event: any): void {
+    var wasPlayingInitially = this.playing
+    if (this.playing)
+      this.pause()
+    this.attackIntervalDelay = event.target.value;
+    if (wasPlayingInitially)
+      this.play();
+  }
+
+
+
+  // PRIVATE METHODS
+
+  private _loadNetworksList() {
+    this.api.listNetworks()
+      .subscribe((response: any) => {
+        this.networks = response['data'];
+      })
+  }
+
+  private _loadCentralitiesList() {
+    this.api.listCentralities()
+      .subscribe((response: any) => {
+        this.centralities = response['data'];
+      })
   }
 
 }
